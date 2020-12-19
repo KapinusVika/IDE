8b719dc897ff2146a13651285ecf078f243b4a5e
Виктория Капинус <viktoriakapinus@MacBook-Pro-Viktoria.local>
2020-12-19 11:31:01 +0300

    updated file_mapper, mybatis, config

--- src/main/java/com/automation/lab/fall/kapinusFilimonov/core22/domain/Version.java
+++ src/main/java/com/automation/lab/fall/kapinusFilimonov/core22/domain/Version.java
@@ -2,44 +2,11 @@ package com.automation.lab.fall.kapinusFilimonov.core22.domain;
 
 import com.automation.lab.fall.kapinusFilimonov.core22.dao.AbstractModel;
 
-import java.util.List;
-import java.util.Objects;
 
 public class Version extends AbstractModel {
-    private List<File> filesList;
 
     public Version(){
     }
 
-    public Version(List<File> filesList) {
-        this.filesList = filesList;
-    }
 
-    public List<File> getFilesList() {
-        return filesList;
-    }
-
-    public void setFilesList(List<File> filesList) {
-        this.filesList = filesList;
-    }
-
-    @Override
-    public String toString() {
-        return "Version{" +
-                "filesList=" + filesList +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Version version = (Version) o;
-        return Objects.equals(filesList, version.filesList);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(filesList);
-    }
 }
